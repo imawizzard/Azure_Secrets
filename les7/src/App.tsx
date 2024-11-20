659bf7af4fc561b8e28b80bd6f77a485d30bcbeb
ima_wizzard <aitor.vannevel@gmail.com>
2024-11-20 09:21:16 +0100

    added background

--- les7/src/App.tsx
+++ les7/src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{ background: process.env.BACKGROUND_COLOR || "" }}
+    >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
